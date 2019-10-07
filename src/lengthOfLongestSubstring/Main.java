e96b089cc4472e4bb4f9d01e5cc7869c32ecd50f
Ping Jianchuan <764463703@qq.com>
2019-10-07 14:53:49 +0800

    Third problem

--- src/lengthOfLongestSubstring/Main.java
+++ src/lengthOfLongestSubstring/Main.java
@@ -1,4 +1,11 @@
 package lengthOfLongestSubstring;
 
 public class Main {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        String str1="abcabcbb";
+        String str2="bbbbb";
+        String str3="pwwkew";
+        System.out.println(solution.lengthOfLongestSubstring(str3));
+    }
 }
--- src/lengthOfLongestSubstring/Solution.java
+++ src/lengthOfLongestSubstring/Solution.java
@@ -1,4 +1,30 @@
 package lengthOfLongestSubstring;
 
 public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        boolean[] alphabeta=new boolean[255];
+
+        int i=0,j=0,maxLength=0;
+        while (j<s.length()) {
+            int diff=s.charAt(j);
+            if (alphabeta[diff]) {
+                while (s.charAt(i)!=s.charAt(j)) {
+                    alphabeta[s.charAt(i)]=false;
+                    i++;
+                }
+                i++;
+                j++;
+                if (j-i > maxLength) {
+                    maxLength=j-i;
+                }
+            } else {
+                alphabeta[diff]=true;
+                j++;
+                if (j-i > maxLength) {
+                    maxLength=j-i;
+                }
+            }
+        }
+        return maxLength;
+    }
 }
